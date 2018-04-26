endlessm/eos-discovery-feed

feed: Port the card querying logic to C
Add some new types to marshal data about the proxies
and returned cards back and forth between libfeed and the
caller.

FeedKnowledgeAppProxy represents a GDBus proxy connected to some
interface that can provide models for the feed, as well as some
additional information, such as what the source of the proxy is
(eg, the app ID or source name), the type of cards that the
proxy produces and the knowledge search interface it exposes.

FeedOrderableModel wraps a EosDiscoveryFeedBaseCardStore and provides
some additional properties such such as the source of the card
and its type. These properties are only used by the ordering algorithm,
at which point the underlying models are unwrapped and returned.

Use eos_discovery_feed_unordered_results_from_queries to asynchronously
query all the relevant knowledge app proxies and return the result
to the provided callback.

https://phabricator.endlessm.com/T21130

libfeed/feed-store-provider.h
@@ -1,0 +1,33 @@
+/* Copyright 2018 Endless Mobile, Inc.
+ *
+ * eos-discovery-feed is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * eos-discovery-feed is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with eos-discovery-feed.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+GPtrArray * eos_discovery_feed_unordered_results_from_queries_finish (GAsyncResult  *result,
+                                                                      GError       **error);
+
+
+void eos_discovery_feed_unordered_results_from_queries (GPtrArray           *ka_proxies,
+                                                        GCancellable        *cancellable,
+                                                        GAsyncReadyCallback  callback,
+                                                        gpointer             user_data);
+
+G_END_DECLS

endlessm/libcontentfeed

feed-store-provider.h
@@ -1,0 +1,33 @@
+/* Copyright 2018 Endless Mobile, Inc.
+ *
+ * eos-discovery-feed is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * eos-discovery-feed is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with eos-discovery-feed.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+GSList * eos_discovery_feed_unordered_results_from_queries_finish (GAsyncResult  *result,
+                                                                   GError       **error);
+
+
+void eos_discovery_feed_unordered_results_from_queries (GPtrArray           *ka_proxies,
+                                                        GCancellable        *cancellable,
+                                                        GAsyncReadyCallback  callback,
+                                                        gpointer             user_data);
+
+G_END_DECLS